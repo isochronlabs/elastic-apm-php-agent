3c1bea4da528f832b981a37bb2759c1fa1bba369
SurveyGizmo Development <SurveyGizmoOfficial@users.noreply.github.com>
2019-10-14 11:20:51 -0600

    More fixes

--- src/Agent.php
+++ src/Agent.php
@@ -21,191 +21,187 @@ use PhilKra\Exception\Transaction\UnknownTransactionException;
  */
 class Agent
 {
-    /**
-     * Agent Version
-     *
-     * @var string
-     */
-    const VERSION = '6.3.2';
+	/**
+	 * Agent Version
+	 *
+	 * @var string
+	 */
+	const VERSION = '6.3.2';
 
-    /**
-     * Agent Name
-     *
-     * @var string
-     */
-    const NAME = 'elastic-php';
+	/**
+	 * Agent Name
+	 *
+	 * @var string
+	 */
+	const NAME = 'elastic-php';
 
-    /**
-     * Config Store
-     *
-     * @var \PhilKra\Helper\Config
-     */
-    private $config;
+	/**
+	 * Config Store
+	 *
+	 * @var \PhilKra\Helper\Config
+	 */
+	private $config;
 
-    /**
-     * Transactions Store
-     *
-     * @var \PhilKra\Stores\TransactionsStore
-     */
-    private $transactionsStore;
+	/**
+	 * Transactions Store
+	 *
+	 * @var \PhilKra\Stores\TransactionsStore
+	 */
+	private $transactionsStore;
 
-    /**
-     * Error Events Store
-     *
-     * @var \PhilKra\Stores\ErrorsStore
-     */
-    private $errorsStore;
+	/**
+	 * Error Events Store
+	 *
+	 * @var \PhilKra\Stores\ErrorsStore
+	 */
+	private $errorsStore;
 
-    /**
-     * Apm Timer
-     *
-     * @var \PhilKra\Helper\Timer
-     */
-    private $timer;
+	/**
+	 * Apm Timer
+	 *
+	 * @var \PhilKra\Helper\Timer
+	 */
+	private $timer;
 
-    /**
-     * Common/Shared Contexts for Errors and Transactions
-     *
-     * @var array
-     */
-    private $sharedContext = [
-      'user'   => [],
-      'custom' => [],
-      'tags'   => []
-    ];
+	/**
+	 * Common/Shared Contexts for Errors and Transactions
+	 *
+	 * @var array
+	 */
+	private $sharedContext = [
+	  'user'   => [],
+	  'custom' => [],
+	  'tags'   => []
+	];
 
-    /**
-     * Setup the APM Agent
-     *
-     * @param array $config
-     * @param array $sharedContext  Set shared contexts such as user and tags
-     *
-     * @return void
-     */
-    public function __construct(array $config, array $sharedContext = [])
-    {
-        // Init Agent Config
-        $this->config = new Config($config);
+	/**
+	 * Setup the APM Agent
+	 *
+	 * @param array $config
+	 * @param array $sharedContext  Set shared contexts such as user and tags
+	 *
+	 * @return void
+	 */
+	public function __construct($config, $sharedContext = [])
+	{
+		// Init Agent Config
+		$this->config = new Config($config);
 
-        // Init the Shared Context
-        $this->sharedContext['user']   = $sharedContext['user'] ?? [];
-        $this->sharedContext['custom'] = $sharedContext['custom'] ?? [];
-        $this->sharedContext['tags']   = $sharedContext['tags'] ?? [];
+		// Init the Shared Context
+		$this->sharedContext['user']   = isset($sharedContext['user']) ? $sharedContext['user'] : [];
+		$this->sharedContext['custom'] = isset($sharedContext['custom']) ? $sharedContext['custom'] : [];
+		$this->sharedContext['tags']   = isset($sharedContext['tags']) ? $sharedContext['tags'] : [];
 
-        // Initialize Event Stores
-        $this->transactionsStore = new TransactionsStore();
-        $this->errorsStore       = new ErrorsStore();
+		// Initialize Event Stores
+		$this->transactionsStore = new TransactionsStore();
+		$this->errorsStore       = new ErrorsStore();
 
-        // Start Global Agent Timer
-        $this->timer = new Timer();
-        $this->timer->start();
-    }
+		// Start Global Agent Timer
+		$this->timer = new Timer();
+		$this->timer->start();
+	}
 
-    /**
-     * Start the Transaction capturing
-     *
-     * @throws \PhilKra\Exception\Transaction\DuplicateTransactionNameException
-     *
-     * @param string $name
-     *
-     * @return Transaction
-     */
-    public function startTransaction(string $name)
-    {
-        // Create and Store Transaction
-        $this->transactionsStore->register(new Transaction($name, $this->sharedContext));
+	/**
+	 * Start the Transaction capturing
+	 *
+	 * @throws \PhilKra\Exception\Transaction\DuplicateTransactionNameException
+	 *
+	 * @param string $name
+	 *
+	 * @return Transaction
+	 */
+	public function startTransaction($name)
+	{
+		// Create and Store Transaction
+		$this->transactionsStore->register(new Transaction($name, $this->sharedContext));
 
-        // Start the Transaction
-        $transaction = $this->transactionsStore->fetch($name);
-        $transaction->start();
-    
-        return $transaction;
-    }
+		// Start the Transaction
+		$transaction = $this->transactionsStore->fetch($name);
+		$transaction->start();
 
-    /**
-     * Stop the Transaction
-     *
-     * @throws \PhilKra\Exception\Transaction\UnknownTransactionException
-     *
-     * @param string $name
-     * @param array $meta, Def: []
-     *
-     * @return void
-     */
-    public function stopTransaction(string $name, array $meta = [])
-    {
-        $this->getTransaction($name)->stop();
-        $this->getTransaction($name)->setMeta($meta);
-    }
+		return $transaction;
+	}
 
-    /**
-     * Get a Transaction
-     *
-     * @throws \PhilKra\Exception\Transaction\UnknownTransactionException
-     *
-     * @param string $name
-     *
-     * @return void
-     */
-    public function getTransaction(string $name)
-    {
-        $transaction = $this->transactionsStore->fetch($name);
-        if ($transaction === null) {
-            throw new UnknownTransactionException($name);
-        }
+	/**
+	 * Stop the Transaction
+	 *
+	 * @throws \PhilKra\Exception\Transaction\UnknownTransactionException
+	 *
+	 * @param string $name
+	 * @param array $meta, Def: []
+	 *
+	 * @return void
+	 */
+	public function stopTransaction($name, $meta = [])
+	{
+		$this->getTransaction($name)->stop();
+		$this->getTransaction($name)->setMeta($meta);
+	}
 
-        return $transaction;
-    }
+	/**
+	 * Get a Transaction
+	 *
+	 * @throws \PhilKra\Exception\Transaction\UnknownTransactionException
+	 *
+	 * @param string $name
+	 *
+	 * @return void
+	 */
+	public function getTransaction($name)
+	{
+		$transaction = $this->transactionsStore->fetch($name);
+		if ($transaction === null) {
+			throw new UnknownTransactionException($name);
+		}
 
-    /**
-     * Register a Thrown Exception, Error, etc.
-     *
-     * @link http://php.net/manual/en/class.throwable.php
-     *
-     * @param \Throwable $thrown
-     *
-     * @return void
-     */
-    public function captureThrowable(\Throwable $thrown)
-    {
-        $this->errorsStore->register(new Error($thrown, $this->sharedContext));
-    }
+		return $transaction;
+	}
 
-    /**
-     * Get the Agent Config
-     *
-     * @return \PhilKra\Helper\Config
-     */
-    public function getConfig()
-    {
-        return $this->config;
-    }
+	/**
+	 * Register a Thrown Exception, Error, etc.
+	 *
+	 * @link http://php.net/manual/en/class.throwable.php
+	 *
+	 * @param \Throwable $thrown
+	 *
+	 * @return void
+	 */
+	public function captureThrowable($thrown)
+	{
+		$this->errorsStore->register(new Error($thrown, $this->sharedContext));
+	}
 
-    /**
-     * Send Data to APM Service
-     *
-     * @return bool
-     */
-    public function send()
-    {
-        // Is the Agent enabled ?
-        if ($this->config->get('active') === false) {
-            return false;
-        }
+	/**
+	 * Get the Agent Config
+	 *
+	 * @return \PhilKra\Helper\Config
+	 */
+	public function getConfig()
+	{
+		return $this->config;
+	}
 
-        $connector = new Connector($this->config);
-        $status = true;
-
-        // Commit the Errors
-        if ($this->errorsStore->isEmpty() === false) {
-            $status = $status && $connector->sendErrors($this->errorsStore);
-        }
-
-        // Commit the Transactions
-        if ($this->transactionsStore->isEmpty() === false) {
-            $status = $status && $connector->sendTransactions($this->transactionsStore);
-        }
-
-        return $status;
-    }
+	/**
+	 * Send Data to APM Service
+	 *
+	 * @return bool
+	 */
+	public function send()
+	{
+		// Is the Agent enabled ?
+		if ($this->config->get('active') === false) {
+			return false;
+		}
+		$connector = new Connector($this->config);
+		$status = true;
+		// Commit the Errors
+		if ($this->errorsStore->isEmpty() === false) {
+			$status = $status && $connector->sendErrors($this->errorsStore);
+		}
+		// Commit the Transactions
+		if ($this->transactionsStore->isEmpty() === false) {
+			$status = $status && $connector->sendTransactions($this->transactionsStore);
+		}
+		return $status;
+	}
 }
