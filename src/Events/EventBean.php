53b8509a2c82f04422536f6eb83785907dccb2e3
Joel Larson <joellarsonweb@gmail.com>
2020-02-12 04:19:39 -0700

    Remove PHP 5.6 dependency from getallheaders

--- src/Events/EventBean.php
+++ src/Events/EventBean.php
@@ -242,7 +242,7 @@ class EventBean
      */
     final public function generateRequest()
     {
-        $headers = getallheaders();
+        $headers = $this->getallheaders();
         $http_or_https = isset($_SERVER['HTTPS']) ? 'https' : 'http';
         $http_version = null;
         if (isset($_SERVER['SERVER_PROTOCOL'])) {
@@ -280,6 +280,42 @@ class EventBean
             'env' => (object)$this->getEnv(),
             'cookies' => (object)$this->getCookies(),
         ];
+    }
+
+    private function getallheaders()
+    {
+        $headers = array();
+
+        $copy_server = array(
+            'CONTENT_TYPE'   => 'Content-Type',
+            'CONTENT_LENGTH' => 'Content-Length',
+            'CONTENT_MD5'    => 'Content-Md5',
+        );
+
+        foreach ($_SERVER as $key => $value) {
+            if (strpos($key, 'HTTP_') === 0) {
+                $key = substr($key, 5);
+                if (!isset($copy_server[$key], $_SERVER[$key])) {
+                    $key = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', $key))));
+                    $headers[$key] = $value;
+                }
+            } elseif (isset($copy_server[$key])) {
+                $headers[$copy_server[$key]] = $value;
+            }
+        }
+
+        if (!isset($headers['Authorization'])) {
+            if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $headers['Authorization'] = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+            } elseif (isset($_SERVER['PHP_AUTH_USER'])) {
+                $basic_pass = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
+                $headers['Authorization'] = 'Basic ' . base64_encode($_SERVER['PHP_AUTH_USER'] . ':' . $basic_pass);
+            } elseif (isset($_SERVER['PHP_AUTH_DIGEST'])) {
+                $headers['Authorization'] = $_SERVER['PHP_AUTH_DIGEST'];
+            }
+        }
+
+        return $headers;
     }
 
     /**
